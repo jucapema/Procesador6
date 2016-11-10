57efbef1ce2bf6279c5337fc35825abf8d95dc1a
camilo <camilo89@utp.edu.co>
2016-11-09 20:07:24 -0500

    Actualizacion

--- Alu.vhd
+++ Alu.vhd
@@ -1,48 +1,90 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use ieee.numeric_std.ALL;
+
 
 entity ALU is
-    Port ( dato1Alu : in  STD_LOGIC_VECTOR (31 downto 0);
-           dato2Alu : in  STD_LOGIC_VECTOR (31 downto 0);
-           operacionAlu : in  STD_LOGIC_VECTOR (5 downto 0);
-           salidaAlu : out  STD_LOGIC_VECTOR (31 downto 0);
-			  carry : in  STD_LOGIC);
+    Port ( CRS1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           CRS2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           ALURESULT : out  STD_LOGIC_VECTOR (31 downto 0);
+			  carry : in  STD_LOGIC;
+           ALUOP : in  STD_LOGIC_VECTOR (5 downto 0));
 end ALU;
 
 architecture Behavioral of ALU is
 
 begin
 
-process(dato1Alu,dato2Alu,operacionAlu ,carry)
-	begin
-		if (operacionAlu  = "000001") or (operacionAlu  = "001001") or (operacionAlu  = "010110") or (operacionAlu  = "010101") then      -- Add or Addcc
-			salidaAlu <= dato1Alu + dato2Alu;
-		elsif (operacionAlu  = "000010") or (operacionAlu  = "001100") then   -- Sub or Subcc
-			salidaAlu <= dato1Alu - dato2Alu;			
-		elsif (operacionAlu  = "000011") or (operacionAlu  = "001111") then	-- And or Andcc 
-			salidaAlu <= dato1Alu and dato2Alu;		
-		elsif (operacionAlu  = "000100") or (operacionAlu  = "010000") then	-- andN or andNcc
-			salidaAlu <= dato1Alu and  not (dato2Alu);			
-		elsif (operacionAlu  = "000101") or (operacionAlu  = "010001") then	-- Or or Orcc 
-			salidaAlu <= dato1Alu or dato2Alu;
-		elsif (operacionAlu  = "000110") or (operacionAlu  = "010010") then	-- orN or orNcc 
-			salidaAlu <= dato1Alu or not (dato2Alu);
-		elsif (operacionAlu  = "000111") or (operacionAlu  = "010010") then	-- Xor or Xorcc
-			salidaAlu <= dato1Alu xor dato2Alu;
-		elsif (operacionAlu  = "001000") or (operacionAlu  = "010100") then	-- Xnor or Xnorcc
-			salidaAlu <= dato1Alu xnor dato2Alu;
-		elsif (operacionAlu  = "001010") or (operacionAlu  = "001011") then	-- Addx or Addxcc
-			salidaAlu <= dato1Alu + dato2Alu + carry;
-		elsif (operacionAlu  = "001101") or (operacionAlu  = "001110") then	-- Subx or Subxcc
-			salidaAlu <= dato1Alu - dato2Alu - carry;
-		elsif (operacionAlu = "010111") then --SLL
-			salidaAlu <= to_stdlogicvector(to_bitvector(dato1Alu) sll conv_integer(dato2Alu));
-		elsif (operacionAlu = "011000") then-- SRL
-			salidaAlu <= to_stdlogicvector(to_bitvector(dato1Alu) srl conv_integer(dato2Alu));
-		else salidaAlu <= (others=>'0');
-		end if;
+	PROCESS(CRS1,CRS2, ALUOP)
+	
+		begin
+		
+			case(ALUOP) is
+				when "000000" => -- add 
+					ALURESULT <= CRS1 + CRS2;
+					
+				when "000101" => -- andn
+					ALURESULT <= CRS1 NAND CRS2;
+					
+				when "000010" =>  --or
+					ALURESULT <= CRS1 OR CRS2;
+					
+				when "000011" => --xor
+					ALURESULT <= CRS1 XOR CRS2;
+				
+				when "000111" => --xnor
+					ALURESULT <= CRS1 XOR not(CRS2);
+					
+				when "000110" => --nor
+					ALURESULT <= CRS1 NOR CRS2;
+					
+				when "000100" => --sub
+					ALURESULT <= CRS1 - CRS2;
+					
+				when "000001" => --and
+					ALURESULT <= CRS1 AND CRS2;
+				
+				when "010001" => --andcc
+					ALURESULT <= CRS1 AND CRS2;
+					
+				when "010101" => --andNcc
+					ALURESULT <= CRS1 NAND CRS2;
+					
+				when "010010" => --ORcc
+					ALURESULT <= CRS1 OR CRS2;
+					
+				when "010110" => --ORNcc
+					ALURESULT <= CRS1 NOR CRS2;--
+					
+				when "010011" => --XORcc
+					ALURESULT <= CRS1 XOR CRS2;--
+			
+				when "010111" => --XORNcc
+					ALURESULT <= CRS1 XNOR CRS2;--
+					
+				when "010000" => --ADDCC
+					ALURESULT <= CRS1 + CRS2;--
+					
+				when "011000" => --ADDXCC
+					ALURESULT <= CRS1 + CRS2 + carry;--
+					
+				when "001000" => --ADDX
+					ALURESULT <= CRS1 + CRS2 + carry;--
+					
+				when "010100" => --SUBCC
+					ALURESULT <= CRS1 - CRS2;--
+					
+				when "001100" => --SUBX
+					ALURESULT <= CRS1 - CRS2 - carry;--
+					
+				when "011100" => --SUBXCC
+					ALURESULT <= CRS1 - CRS2 - carry;--
+					
+				when others => -- Cae el nop
+					AluResult <= (others=>'0');	
+					
+			 end case;		
+		
 	end process;
 
 end Behavioral;
